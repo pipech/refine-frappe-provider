c08240de65fc5061b234550db52ca8f6adc93747
Poranut Chollavorn <pipech@gmail.com>
2024-03-06 12:22:41 +0000

    fix: allow `options` props to be pass to `accessControlProvider`

--- src/accessControl/accessControlProvider.ts
+++ src/accessControl/accessControlProvider.ts
@@ -2,14 +2,22 @@ import {
   type AccessControlProvider,
 } from "@refinedev/core";
 
-import AccessControlClient, { AccessControlParams } from "./accessControlClient";
+import AccessControlClient, {
+  type AccessControlParams,
+  type AccessControlProviderParams,
+} from "./accessControlClient";
 
 const accessControlProvider = (
-  params: AccessControlParams,
+  params: AccessControlParams & AccessControlProviderParams,
 ): AccessControlProvider => {
-  const client = new AccessControlClient(params);
+  const {
+    options = {},
+    ...clientParams
+  } = params;
 
-  return client.provider();
+  const client = new AccessControlClient(clientParams);
+
+  return client.provider({ options });
 };
 
 export default accessControlProvider;
